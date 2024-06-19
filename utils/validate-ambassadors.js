c880b3ee31249ba2c0057f7efc1e9e6bcfa04cb8
ramishj <118445402+ramishj@users.noreply.github.com>
2024-06-19 16:18:54 +0530

    Added Workflow to verify ambassaor.json schema

--- utils/validate-ambassadors.js
+++ utils/validate-ambassadors.js
@@ -1,20 +1,22 @@
-// validate-ambassadors.js
+// utils/validate-ambassadors.js
 const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
 const path = require('path');
 const fs = require('fs');
 
 // Set the directory where ambassadors.json and ambassadors-schema.json reside
-const ambassadorsDir = path.join(__dirname, '../programs', 'ambassadors');
+const ambassadorsDir = path.join(__dirname, '../programs/ambassadors');
 const schemaPath = path.join(ambassadorsDir, 'ambassadors-schema.json');
 const dataPath = path.join(ambassadorsDir, 'ambassadors.json');
 
 try {
     // Load schema and data files
-    const schema = require(schemaPath);
-    const data = require(dataPath);
+    const schema = JSON.parse(fs.readFileSync(schemaPath, 'utf-8'));
+    const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
     // Initialize AJV instance with options
     const ajv = new Ajv({ allErrors: true }); // Validate all errors, not just the first one
+    addFormats(ajv); // Add support for additional formats
 
     // Compile the schema
     const validate = ajv.compile(schema);
